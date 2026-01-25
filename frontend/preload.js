c5d03ac45387c4b95fb15957700f4d14f0b869b6
Rohan-Shah-312003 <rohan312003@gmail.com>
2026-01-25 13:09:18 +0530

    major UI update

--- frontend/preload.js
+++ frontend/preload.js
@@ -1,24 +1,3 @@
-// const { contextBridge } = require("electron");
-
-// contextBridge.exposeInMainWorld("api", {
-// 	analyzeIncident: async (text) => {
-// 		const response = await fetch("http://127.0.0.1:8000/analyze", {
-// 			method: "POST",
-// 			headers: {
-// 				"Content-Type": "application/json",
-// 			},
-// 			body: JSON.stringify({ incident_text: text }),
-// 		});
-
-// 		if (!response.ok) {
-// 			throw new Error("Backend error");
-// 		}
-
-// 		return response.json();
-// 	},
-// });
-
-
 const { contextBridge } = require("electron");
 // const { marked } = require("marked");
 
